781988801215e89b7b4bdf0ef96e31b8d069aa16
Ooops <dev.seb.3745@gmail.com>
2025-07-30 21:03:34 -0500

    config and orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import serverConfig from 'config/server.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+import databaseConfig from 'config/database.config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync(
+      databaseConfig.asProvider() as Partial<PostgresConnectionOptions>,
+    ),
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      load: [serverConfig],
+      isGlobal: true,
+      cache: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
